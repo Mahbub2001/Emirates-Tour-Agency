be5876cd8117fce578e7ce8ff8603ed728cb068a
Mahbub2001 <imahbubturza@gmail.com>
2022-10-23 00:00:01 +0600

    errorpage

---
+++ src/components/SpecialOffers.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SpecialOffers = () => {
+    return (
+        <div className='flex flex-row justify-center destination-container'>
+            <div className='destination-inner'>
+                
+            </div>
+        </div>
+    );
+};
+
+export default SpecialOffers;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./Home.Module.css";
 import HomePicture from "../../assets/coxsBajar.png";
+import SpecialOffers from "../../components/SpecialOffers";
 
 const Home = () => {
   const [date, setDate] = useState();
@@ -11,18 +12,21 @@ const Home = () => {
       <div className="home-container mb-4">
         <img className="home-picture" src={HomePicture} alt="" />
       </div>
-      <div className="flex justify-center destination-container">
+      <div className="flex flex-row justify-center destination-container">
         <div className="shadow-xl destination-inner">
           <form className="destination-form grid lg:grid-cols-4 md:grid-cols-1 lg:justify-center lg:justify-items-center items-center md:pl-10">
-
-            <div className="relative">
+            <div className="rln">
               <h4>From</h4>
               <input
                 className="destination"
                 name="destination"
                 type="text"
                 placeholder="Destination"
-              />
+              />          
+             <hr  className="hr"/>
+            </div>
+
+            <div className="rln">
               <h4 className="mt-2">To</h4>
               <input
                 className="destination"
@@ -30,10 +34,10 @@ const Home = () => {
                 type="text"
                 placeholder="Destination"
               />
-              <span className="vertical-1"></span>
+              <hr  className="hr"/>
             </div>
 
-            <div className="date-pickerdiv relative">
+            <div className="date-pickerdiv rln">
               <h4>Date:</h4>
               <input
                 className="destination date-picker"
@@ -41,20 +45,21 @@ const Home = () => {
                 name="date-picker"
                 onChange={(e) => setDate(e.target.value)}
               />
-              <span className="vertical-2"></span>
+              <hr  className="hr"/>
             </div>
 
-            <div className="relative">
+            <div className="">
               <p>Persons</p>
-              <input type="text" className="destination" placeholder="Write here"/>
-              <span className="vertical-2"></span>
+              <input
+                type="number"
+                className="destination"
+                placeholder="Write here"
+              />
             </div>
-
-            <div className=""></div>
-            
           </form>
         </div>
       </div>
+     <SpecialOffers/>
     </div>
   );
 };
